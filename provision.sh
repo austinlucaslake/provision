146c8e875870bc1a662ab7df40a80a152f7936fd
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-11-06 15:04:52 -0800

    Removed unnessary async from apt install

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt install -y ansible
+sudo apt install -y --no-instal-recommends ansible
 ansible-galaxy collection install community.general community.crypto
 curl https://raw.githubusercontent.com/austinlucaslake/provision/main/provision.yaml | ansible-playbook /dev/stdin -e "token=$1"
