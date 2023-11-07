1fe012e5eacabe0590594b871a9a460c2df57370
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-11-06 16:47:09 -0800

    Add passthrough for SSH and GPG passphrases

--- provision.sh
+++ provision.sh
@@ -2,4 +2,4 @@
 
 sudo apt install -y --no-instal-recommends ansible
 ansible-galaxy collection install community.general community.crypto
-curl https://raw.githubusercontent.com/austinlucaslake/provision/main/provision.yaml | ansible-playbook /dev/stdin -e "token=$1"
+curl https://raw.githubusercontent.com/austinlucaslake/provision/main/provision.yaml | ansible-playbook /dev/stdin -e "token=$1 sshpassphrase=$2 gpgpassphrase=$3"
